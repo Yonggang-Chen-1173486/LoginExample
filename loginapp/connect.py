587e7307a4026fcdf1e9e77f59688093b680a012
Yonggang-Chen-1173486 <Yonggang.Chen@lincolnuni.ac.nz>
2026-02-26 01:14:23 +0700

    testing

--- loginapp/connect.py
+++ loginapp/connect.py
@@ -29,7 +29,7 @@ On PythonAnywhere:
     "user1234$loginexample".
 """
 dbuser = 'postgres'  # PUT YOUR USERNAME HERE - usually "root"
-dbpass = 'cYG13450806545'  # PUT YOUR PASSWORD HERE
+dbpass = 'WRONGPASSWORD'  # PUT YOUR PASSWORD HERE
 dbhost = 'localhost'
 dbport = 5432
 dbname = 'loginexample'
